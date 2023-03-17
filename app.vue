3691ea5cfb47437fa7045fcbdb3f789e522b0dfa
Austin Lanari <austin@jumanji.io>
2023-03-17 10:35:04 -0700

    update copy and add dockyard info

--- app.vue
+++ app.vue
@@ -19,8 +19,8 @@
     <div class='mb-6 grid grid-cols-3 gap cols'>
         <time class='col-span-1'>April 2022 - </time>
         <div class='col-span-2'>
-            <span class='underline mr-4'>Senior Elixir Consultant (contract)</span>
-            <span class='text-slate-500'>Feature development for Fortune 500 company</span>
+            <span class='underline'><a rel="noreferrer" target="_blank" href='https://dockyard.com'>Dockyard</a></span><span class='mr-4'> (contract)</span>
+            <span class='text-slate-500'>Senior Elixir Consultant on a Dockyard-led team for their Adobe contract</span>
         </div>
       <time class='col-span-1'>2021 - 2022</time>
         <div class='col-span-2'>
@@ -34,7 +34,7 @@
       </div>
     </div>
     <div class='mb-6 space-y-4'>
-      <p>Software engineer especially comfortable with devops, quality assurance, and distributed systems via Elixir/OTP.</p>
+      <p>Software engineer with large scale production experience of Elixir-powered backends, React-powered frontends, and more. Excited about making things that work for users and future developers.</p>
       <p>Published non-fiction in the Eisner-nominated <a class='underline' el="noreferrer" target="_blank" href='https://www.fantagraphics.com/products/the-comics-journal-304'>The Comics Journal #304</a>.</p>
     </div>
     <div class='underline'>
